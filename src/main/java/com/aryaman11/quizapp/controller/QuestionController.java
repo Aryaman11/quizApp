909fcfc58fe8c50b22a21749121ef987532b95ec
Aryaman <aryamanyash11@gmail.com>
2024-06-02 16:40:13 +0530

    changes in mapping , added get , post , delete method

--- src/main/java/com/aryaman11/quizapp/controller/QuestionController.java
+++ src/main/java/com/aryaman11/quizapp/controller/QuestionController.java
@@ -4,8 +4,12 @@ import com.aryaman11.quizapp.Question;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.*;
 
@@ -16,15 +20,26 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("getAllQuestion")
-    public List<Question> getAllQuestion(){
+    public ResponseEntity<List<Question>> getAllQuestion(){
         return questionService.getAllQuestion();
     }
 
+    @GetMapping("delete/{id}")
+    public ResponseEntity<String> deleteQuestion(@PathVariable("id") int id){
+        return questionService.deleteQuestion(id);
+    }
+
     @GetMapping("category/{cat}")
-    public List <Question> categoryAll(@PathVariable String cat){ {
+    public ResponseEntity<List <Question>> categoryAll(@PathVariable String cat){ 
         return questionService.categoryAll(cat);
     }
     
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+           return questionService.addQuestion(question);
+    }
+
+    
     
     }
-}
+
--- src/main/java/com/aryaman11/quizapp/service/QuestionService.java
+++ src/main/java/com/aryaman11/quizapp/service/QuestionService.java
@@ -1,8 +1,9 @@
 package com.aryaman11.quizapp.service;
 import com.aryaman11.quizapp.Question;
 import com.aryaman11.quizapp.dao.QuestionDao;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import java.util.*;
 @Service
@@ -10,11 +11,43 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Question> getAllQuestion(){
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestion(){
+        try{
+           return new ResponseEntity<>(questionDao.findAll(),HttpStatus.OK);
+        }catch(Exception e){
+           e.printStackTrace();
+           return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public List<Question> categoryAll(String category){
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> categoryAll(String category){
+        try{
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();    
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<String> addQuestion(Question question){
+        try{
+         questionDao.save(question);
+        return new ResponseEntity<>("Question added successfully",HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+    }
+
+    public ResponseEntity<String> deleteQuestion(int id){
+        try{
+            questionDao.deleteById(id);
+        return new ResponseEntity<>("Question deleted successfully",HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
     }
 }
